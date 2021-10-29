c5a2875c4039af0212799c5b08caa5bd83220e83
michaelparkernorton <72527646+michaelparkernorton@users.noreply.github.com>
2021-10-29 08:06:34 -0400

    Update HelloFromNamukwaya.java

--- src/week4/HelloFromNamukwaya.java
+++ src/week4/HelloFromNamukwaya.java
@@ -4,5 +4,6 @@ public class HelloFromNamukwaya {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Namukwaya");
+        System.out.println("Hello, my name is Michael");
     }
 }
